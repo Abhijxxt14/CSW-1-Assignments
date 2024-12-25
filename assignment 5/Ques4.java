97ce98f121c712e7581ec66f7c6cb8b1f1d59948
Abhijeet soren <154517350+Abhijxxt14@users.noreply.github.com>
2024-12-25 12:33:44 +0530

    Create Ques4.java

---
+++ assignment 5/Ques4.java
@@ -0,0 +1,11 @@
+
+class Main {
+    public static void main(String[] args) {
+        boolean num = true;
+        Boolean numInt = new Boolean(num);//constructor
+        System.out.println(numInt);
+        boolean newNum = false;
+        Boolean lol = newNum;//autoboxing
+        System.out.println(newNum);
+    }
+} 
