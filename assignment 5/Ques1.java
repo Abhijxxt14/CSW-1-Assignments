fe006af6fa6f920f054af7a4ae9188cea2e81bc1
Abhijeet soren <154517350+Abhijxxt14@users.noreply.github.com>
2024-12-25 12:28:17 +0530

    Create Ques1.java

---
+++ assignment 5/Ques1.java
@@ -0,0 +1,10 @@
+class Main {
+    public static void main(String[] args) {
+        int num = 69;
+        Integer numInt = new Integer (num);//constructor
+        System.out.println(numInt);
+        int newNum = 96;
+        Integer lol = newNum;//autoboxing
+        System.out.println(newNum);
+    }
+} 
