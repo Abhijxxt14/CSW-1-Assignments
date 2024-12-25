153cb339ca51e036b5887de76d7a293eaf79fb43
Abhijeet soren <154517350+Abhijxxt14@users.noreply.github.com>
2024-12-25 12:31:45 +0530

    Create Ques3.java

---
+++ assignment 5/Ques3.java
@@ -0,0 +1,12 @@
+
+
+class Main {
+    public static void main(String[] args) {
+        double num = 69.6463;
+        Double numInt = new Double (num);//constructor
+        System.out.println(numInt);
+        double newNum = 96.34546;
+        Double lol = newNum;//autoboxing
+        System.out.println(newNum);
+    }
+} 
