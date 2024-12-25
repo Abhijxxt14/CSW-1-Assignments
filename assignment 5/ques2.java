3089ce5417034d0fec4816adb36a8f8f28c7e962
Abhijeet soren <154517350+Abhijxxt14@users.noreply.github.com>
2024-12-25 12:29:46 +0530

    Create ques2.java

---
+++ assignment 5/ques2.java
@@ -0,0 +1,10 @@
+class Main {
+    public static void main(String[] args) {
+        float num = 69f;
+        Float numInt = new Float (num);//constructor
+        System.out.println(numInt);
+        float newNum = 96f;
+        Float lol = newNum;//autoboxing
+        System.out.println(newNum);
+    }
+} 
