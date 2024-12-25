2128f6265fbf5796a7df42d984506dbad50792c6
Abhijeet soren <154517350+Abhijxxt14@users.noreply.github.com>
2024-12-25 12:46:26 +0530

    Create Ques8.java

---
+++ assignment 5/Ques8.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+class Main {
+    public static void main(String[] args) {
+Scanner sc = new Scanner (System.in);
+System.out.println("entertrue or false");
+String blean = sc.nextLine();
+Boolean val = Boolean.parseBoolean(blean);
+System.out.println(val);
+
+    }
+} 
